0f5d7b11400938e6b529ce17332634816120f443
Elias <elissdolenz@gmail.com>
2025-12-13 19:35:15 -0300

    Finalización de API RESTful, ahora avanzaremos con las pruebas unitarias

---
+++ src/main/java/APIGestorDeVehiculos/controller/UsageController.java
@@ -0,0 +1,46 @@
+package APIGestorDeVehiculos.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import APIGestorDeVehiculos.domain.Usage;
+import APIGestorDeVehiculos.service.UsageService;
+
+@RestController
+@RequestMapping("/api/uso")
+public class UsageController {
+    private final UsageService usageService;
+
+    public UsageController(UsageService usageService) {
+        this.usageService = usageService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Usage> checkIn(@RequestBody Usage usage) {
+        Usage newUsage = this.usageService.checkIn(usage);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(newUsage.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(newUsage);
+    }
+
+    @PatchMapping("/{usageId}/checkout")
+    public ResponseEntity<Usage> checkOut(@PathVariable Long idUsage, @RequestBody Integer endKm){
+        try {
+            Usage finishUsage = this.usageService.CheckOut(idUsage, endKm);
+            return ResponseEntity.ok(finishUsage);
+        } catch (IllegalStateException | IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+}
---
+++ src/main/java/APIGestorDeVehiculos/service/UsageService.java
@@ -0,0 +1,53 @@
+package APIGestorDeVehiculos.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import APIGestorDeVehiculos.domain.Usage;
+import APIGestorDeVehiculos.domain.Vehicle;
+import APIGestorDeVehiculos.repository.UsageRepository;
+
+@Service
+public class UsageService {
+    private final UsageRepository usageRepository;
+    private final VehicleService vehicleService;
+
+    public UsageService(UsageRepository usageRepository, VehicleService vehicleService) {
+        this.usageRepository = usageRepository;
+        this.vehicleService = vehicleService;
+    }
+
+    public Usage checkIn(Usage newUsage) {
+        if (newUsage.getVehicle() == null) {
+            throw new IllegalArgumentException("Vehiculo no valido para el check-in.");
+        }
+        newUsage.setStartTime(LocalDateTime.now());
+
+        return this.usageRepository.save(newUsage);
+    }
+
+    @Transactional
+    public Usage CheckOut(Long usageId, Integer endKm) {
+        Usage usage = this.usageRepository.findById(usageId)
+                .orElseThrow(() -> new IllegalStateException("Uso no encontrado o ya finalizado."));
+
+        if (usage.getEndTime() != null) {
+            throw new IllegalStateException("El uso ya ha sido finalizado");
+        }
+        if (endKm <= usage.getStartKm()) {
+            throw new IllegalArgumentException("El kilometraje final debe ser mayor que el inicial");
+        }
+
+        usage.setEndKm(endKm);
+        usage.setEndTime(LocalDateTime.now());
+        Usage finishedUsage = usageRepository.save(usage);
+
+        Vehicle vehicle = finishedUsage.getVehicle();
+        vehicle.setCurrentKm(endKm);
+        vehicleService.saveVehicle(vehicle);
+
+        return finishedUsage;
+    }
+}
